58fc9ec1e14ddbe180aa2a87bc853e58ab13cac2
LuisGustavoRosa <luis.cardoso@rede.ulbra.br>
2022-09-23 08:38:53 -0300

    finalizado exame de saude com healthCheck

--- superseller/start/routes.ts
+++ superseller/start/routes.ts
@@ -1,4 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
 Route.resource('/sellers', 'SellersController')
 .apiOnly()
@@ -16,3 +17,11 @@ Route.post('/addLeadSeller', 'LeadsController.addLeadSeller')
 
 Route.get('/indexLeadNoSeller','LeadsController.indexLeadNoSeller')
 Route.post('/signedLead/:id','LeadsController.signedLead')
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy
+    ? response.ok(report)
+    : response.badRequest(report)
+})
